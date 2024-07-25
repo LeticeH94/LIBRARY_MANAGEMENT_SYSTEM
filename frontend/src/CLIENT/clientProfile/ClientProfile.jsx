9b8bb74ebd9f39f9b3aaa494f575ea1525307b4c
LAETITIA HINOUT <lhinout@yahoo.fr>
2024-07-25 09:50:57 +0200

    updating project

--- frontend/src/CLIENT/clientProfile/ClientProfile.jsx
+++ frontend/src/CLIENT/clientProfile/ClientProfile.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { useLoginState } from '../../LoginState'
-import { Col, Row } from 'react-bootstrap'
-import axios from 'axios'
-import { backend_server } from '../../main'
-import ClientDashboard from './ClientDashboard'
-import ClientDetails from './ClientDetails'
-import ClientLogout from '../clientLogout/ClientLogout'
-import './clientprofile.css'
+import React, { useState, useEffect } from "react";
+import { useLoginState } from "../../LoginState";
+import { Col, Row } from "react-bootstrap";
+import axios from "axios";
+import { backend_server } from "../../main";
+import ClientDashboard from "./ClientDashboard";
+import ClientDetails from "./ClientDetails";
+import ClientLogout from "../clientLogout/ClientLogout";
+import "./clientprofile.css";
 
 const ClientProfile = () => {
-  const userLoginState = useLoginState()
+  const userLoginState = useLoginState();
 
-  const getSingleUser_API_URL = `${backend_server}/api/v1/users/`
+  const getSingleUser_API_URL = `${backend_server}/api/v1/users/`;
 
-  const [userBookData, setUserBookData] = useState([])
-  const [userData, setUserData] = useState()
+  const [userBookData, setUserBookData] = useState([]);
+  const [userData, setUserData] = useState();
 
   // Using post to send Cookie and fetch user data
-  const fetchData = async () => {
+  /* const fetchData = async () => {
     try {
       const response = await axios.post(getSingleUser_API_URL, {})
 
@@ -34,63 +34,63 @@ const ClientProfile = () => {
       console.log(error.response)
       console.log(error)
     }
-  }
+  }*/
 
   useEffect(() => {
-    fetchData()
-  }, [userBookData, userData])
+    fetchData();
+  }, [userBookData, userData]);
 
-  const [showDashboard, setShowDashboard] = useState(true)
-  const [showProfile, setShowProfile] = useState(false)
-  const [showLogout, setShowLogout] = useState(false)
+  const [showDashboard, setShowDashboard] = useState(true);
+  const [showProfile, setShowProfile] = useState(false);
+  const [showLogout, setShowLogout] = useState(false);
 
   const handleOnclickDashboard = () => {
-    setShowDashboard(true)
-    setShowProfile(false)
-    setShowLogout(false)
-  }
+    setShowDashboard(true);
+    setShowProfile(false);
+    setShowLogout(false);
+  };
   const handleOnclickProfile = () => {
-    setShowProfile(true)
-    setShowDashboard(false)
-    setShowLogout(false)
-  }
+    setShowProfile(true);
+    setShowDashboard(false);
+    setShowLogout(false);
+  };
   const handleOnclickLogout = () => {
-    setShowLogout(true)
-    setShowDashboard(false)
-    setShowProfile(false)
-  }
+    setShowLogout(true);
+    setShowDashboard(false);
+    setShowProfile(false);
+  };
 
   return (
-    <div className='container-fluid'>
+    <div className="container-fluid">
       {/* <Row className='my-3'>
         <h1 className='h1 text-center'>
           Welcome, {userLoginState.userLogState}
         </h1>
       </Row> */}
 
-      <Row className='row client-sidebar'>
+      <Row className="row client-sidebar">
         {/* Left Bar */}
-        <Col className='col-md-2 client-dashboard-buttons'>
+        <Col className="col-md-2 client-dashboard-buttons">
           <button
             onClick={handleOnclickDashboard}
-            className='btn btn-primary my-1 mx-1'
-            style={{ width: '100%' }}
+            className="btn btn-primary my-1 mx-1"
+            style={{ width: "100%" }}
           >
             Dashboard
           </button>
 
           <button
             onClick={handleOnclickProfile}
-            className='btn btn-primary my-1 mx-1'
-            style={{ width: '100%' }}
+            className="btn btn-primary my-1 mx-1"
+            style={{ width: "100%" }}
           >
             My Details
           </button>
 
           <button
-            className='btn btn-primary my-1 mx-1'
+            className="btn btn-primary my-1 mx-1"
             onClick={handleOnclickLogout}
-            style={{ width: '100%' }}
+            style={{ width: "100%" }}
           >
             Logout
           </button>
@@ -120,7 +120,7 @@ const ClientProfile = () => {
         )}
       </Row>
     </div>
-  )
-}
+  );
+};
 
-export default ClientProfile
+export default ClientProfile;
